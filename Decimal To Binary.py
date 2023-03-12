f211832ae88acc0c5f87b3cb495b171ccdc25697
Ally <allystellaomara@gmail.com>
2023-03-12 17:42:38 +1100

    Add convert decimal to binary using loops

--- Decimal To Binary.py
+++ Decimal To Binary.py
@@ -1,97 +1,36 @@
-#Get input from user & convert into integers (in decimal)
-INPUT = input("Enter number to convert to binary: ")
-binary = 0
-num = int(INPUT)
+# Helper Functions
 
-#Convert into binary
+def getPower(strLocation, strLength):
+    return strLength - strLocation - 1
 
+def caluclatePowerOfTwo(power):
+    return 2**power
 
+# Get input from user
+#userInput = input("Enter number to convert to binary: ")
+userInput = "3"
 
+inputLength = len(userInput)
+binaryOutput = ""
+binaryLength = 4 # Length of binary string
+ 
+# Convert input from string to integer
+userInputAsDec = int(userInput)
 
-if (num - 1024) >= 0:
-    binary = binary + 10000000000
-    num = num - 1024
-else:
-    binary = binary
-    num = num
+binaryRef = 0
+# Convert input to binary
+while binaryRef < binaryLength:
+    
+    # Check if the binaryRef position will divide into userInput
+    power = getPower(binaryRef, binaryLength)
+    integerAmt = caluclatePowerOfTwo(power)
 
-if (num - 512) >= 0:
-    binary = binary + 1000000000
-    num = num - 512
-else:
-    binary = binary
-    num = num
+    if userInputAsDec // integerAmt != 0:
+    #if userInputAsDec - integerAmt >= 0:
+        binaryOutput += "1" 
+    else:
+        binaryOutput += "0" 
 
-if (num - 256) >= 0:
-    binary = binary + 100000000
-    num = num - 256
-else:
-    binary = binary
-    num = num
+    binaryRef += 1
 
-if (num - 128) >= 0:
-    binary = binary + 10000000
-    num = num - 128
-else:
-    binary = binary
-    num = num
-
-if (num - 64) >= 0:
-    binary = binary + 1000000
-    num = num - 64
-else:
-    binary = binary
-    num = num
-
-if (num - 32) >= 0:
-    binary = binary + 100000
-    num = num - 32
-else:
-    binary = binary
-    num = num
-
-if (num - 16) >= 0:
-    binary = binary + 10000
-    num = num - 16
-else:
-    binary = binary
-    num = num
-
-if (num - 8) >= 0:
-    binary = binary + 1000
-    num = num - 8
-else:
-    binary = binary
-    num = num
-
-if (num - 4) >= 0:
-    binary = binary + 100
-    num = num - 4
-else:
-    binary = binary
-    num = num
-
-if (num - 4) >= 0:
-    binary = binary + 100
-    num = num - 4
-else:
-    binary = binary
-    num = num
-
-if (num - 2) >= 0:
-    binary = binary + 10
-    num = num - 2
-else:
-    binary = binary
-    num = num
-
-if (num - 1) >=0:
-    binary = binary + 1
-    num = num - 1
-else:
-    binary = binary
-    num = num
-
-
-#Print
-print("The number " + str(INPUT) + " converted to binary is: " + str(binary))
+print(binaryOutput)
